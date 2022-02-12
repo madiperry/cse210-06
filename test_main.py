0d952ba3f9ed6642f7acbc5056ae138b4f962602
JermainLopez <97272097+JermainLopez@users.noreply.github.com>
2022-02-12 16:55:51 -0600

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,7 +5,7 @@ from game.shared.color import Color
 from game.shared.point import Point
 import pytest
 
-class Test4:
+class Test_car:
     """class to test the cast functions of main."""
     #Test Cast
 
@@ -18,24 +18,24 @@ class Test4:
         for x in range(5):
             for y in range(5):
                 cast = Cast()
-                snake_1 = Actor()
+                car_1 = Actor()
                 position = Point(x, y)
-                snake_1.set_text("S1")
-                snake_1.set_font_size(3)
-                snake_1.set_color("blue")
-                snake_1.set_position(position)
-                cast.add_actor("Snake1", snake_1)
-                actor1 = cast.get_actors("Snake1")
+                car_1.set_text("S1")
+                car_1.set_font_size(3)
+                car_1.set_color("blue")
+                car_1.set_position(position)
+                cast.add_actor("car1", car_1)
+                actor1 = cast.get_actors("car1")
 
                 cast2 = Cast()
-                snake_2 = Actor()
+                Car_ = Actor()
                 position2 = Point(y + x, x * y)
-                snake_2.set_text("S2")
-                snake_2.set_font_size(3)
-                snake_2.set_color("red")
-                snake_2.set_position(position2)
-                cast2.add_actor("Snake2", snake_2)
-                actor2 = cast2.get_actors("Snake2")
+                Car_.set_text("S2")
+                Car_.set_font_size(3)
+                Car_.set_color("red")
+                Car_.set_position(position2)
+                cast2.add_actor("car2", Car_)
+                actor2 = cast2.get_actors("car2")
                 assert actor1 != actor2
     #actor test
 
@@ -54,9 +54,9 @@ class Test4:
         test_get_font_size():
         This method verifies that the actor returns the ideal size for artifacts.
         """
-        snake1 = Actor()
-        snake2 = Actor()
-        assert snake1.get_font_size() == snake2.get_font_size() 
+        car_1 = Actor()
+        car_2 = Actor()
+        assert car_1.get_font_size() == car_2.get_font_size() 
 
 
 
@@ -65,15 +65,15 @@ class Test4:
         test_get_text():
         This method verifies that the actor returns the desired text to be used for subsequent groups of artifacts.
         """
-        snakes = ["Snake1", "Snake2", "Snake3", "Snake4", "Snake5", "Snake6"]
+        car_list = ["car1", "car2", "car3", "car4", "car5", "car6"]
         list_last_names = ["Owen", "Lopez", "Terwilleger", "Hoskins", " Hartfield", "Platt"]
-        for i in range(len(snakes)):
-            snake_1 = Actor()
-            snake_1.set_text(snakes[i])
-            text1 = snake_1.get_text()
-            snake_2 = Actor()
-            snake_2.set_text(list_last_names[i])
-            text2 = snake_2.get_text()
+        for i in range(len(car_list)):
+            car_1 = Actor()
+            car_1.set_text(car_list[i])
+            text1 = car_1.get_text()
+            car_2 = Actor()
+            car_2.set_text(list_last_names[i])
+            text2 = car_2.get_text()
             assert text1 != text2
 
 
@@ -83,8 +83,8 @@ class Test4:
         This method verifies that the velocity and position are always different at a point in the plane.
         """
         for i in range(50):
-            sanke = Actor()
-            assert sanke.get_velocity()  != sanke.get_position()  
+            car_actor = Actor()
+            assert car_actor.get_velocity()  != car_actor.get_position()  
     
     def print_test3(self):
         """printing out the results"""
@@ -96,5 +96,5 @@ class Test4:
          
         pytest.main(["-v", "--tb=line", "-rN", __file__])
 
-main_test = Test4()
+main_test = Test_car()
 main_test.print_test3()
