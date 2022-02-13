6cf240cbf5d5f0468eadbce1e650119002926393
joshowen04 <owe21015@byui.edu>
2022-02-12 23:14:23 -0300

    incrementing score

--- game/scripting/handle_collisions_action.py
+++ game/scripting/handle_collisions_action.py
@@ -7,7 +7,7 @@ from game.shared.point import Point
 class HandleCollisionsAction(Action):
     """
     An update action that handles interactions between the actors.
-    
+
     The responsibility of HandleCollisionsAction is to handle the situation when the snake collides
     with the food, or the snake collides with its segments, or the game is over.
 
@@ -31,10 +31,11 @@ class HandleCollisionsAction(Action):
             self._handle_food_collision(cast)
             self._handle_segment_collision(cast)
             self._handle_game_over(cast)
+            self._handle_top_collision(cast)
 
     def _handle_food_collision(self, cast):
         """Updates the score nd moves the food if the snake collides with the food.
-        
+
         Args:
             cast (Cast): The cast of Actors in the game.
         """
@@ -46,7 +47,7 @@ class HandleCollisionsAction(Action):
         snake2 = cast.get_second_actor("snakes")   # added
         head = snake.get_segments()[0]
         head2 = snake2.get_segments()[0]   # added
-        
+
         if head.get_position().equals(nitro.get_position()):
             nitro.accelerate()
             nitro.reset()
@@ -55,37 +56,39 @@ class HandleCollisionsAction(Action):
             nitro.reset()
         if head.get_position().equals(obstacle.get_position()):
             obstacle.accelerate()
-            obstacle.reset() 
+            obstacle.reset()
 
         if head2.get_position().equals(obstacle.get_position()):   # added
-            points = obstacle.get_points()                          # added     
-            score2.add_points(points)                                # added       
-            obstacle.reset()                                        # added         
+            points = obstacle.get_points()                          # added
+            score2.add_points(points)                                # added
+            obstacle.reset()                                        # added
 
     def _handle_top_collision(self, cast):
         """Updates the score when car collides with the top of screen.
-        
+
         Args:
             cast (Cast): The cast of Actors in the game.
         """
         score1 = cast.get_first_actor("scores")
         score2 = cast.get_second_actor("scores")
         snake = cast.get_first_actor("snakes")
-        snake2 = cast.get_second_actor("snakes")  
-        top = constants.MAX_Y
-       
+        snake2 = cast.get_second_actor("snakes")
+
+        head = snake.get_head()
+        head2 = snake2.get_head() # added
+        top = 0
         
-        if snake.get_position().equals(top):
-             points = top.get_points()
-             score1.add_points(points)
-             
-        if snake2.get_position().equals(top):
-             points = top.get_points()
-             score2.add_points(points)
+        if head.get_position().equals2(top):
+                points = 1
+                score1.add_points(points)
+
+        if head2.get_position().equals2(top):
+                points = 1
+                score2.add_points(points)
 
     def _handle_segment_collision(self, cast):
         """Sets the game over flag if the snake collides with one of its segments.
-        
+
         Args:
             cast (Cast): The cast of Actors in the game.
         """
@@ -101,7 +104,7 @@ class HandleCollisionsAction(Action):
         segments2 = snake2.get_segments()[1:] # added
 
         for segment in segments:
-            if head2.get_position().equals(segment.get_position()):     # If snake 2 hits snake 1 
+            if head2.get_position().equals(segment.get_position()):     # If snake 2 hits snake 1
                 score2.add_points(-50)
                 self._is_game_over = True
                 if score1.get_points() > score2.get_points():
@@ -129,7 +132,7 @@ class HandleCollisionsAction(Action):
                 elif score1.get_points() == score2.get_points():
                     self.winner = 3
                 else:
-                    self.winner = 2         
+                    self.winner = 2
             if head2.get_position().equals(segment.get_position()):    # if snake 2 hits itself
                 score2.add_points(-50)
                 self._is_game_over = True
@@ -143,11 +146,11 @@ class HandleCollisionsAction(Action):
 
     def _handle_game_over(self, cast):
         """Shows the 'game over' message and turns the snake and food white if the game is over.
-        
+
         Args:
             cast (Cast): The cast of Actors in the game.
         """
-        
+
         if self._is_game_over:
             snake = cast.get_first_actor("snakes")
             snake2 = cast.get_second_actor("snakes") # added
@@ -157,7 +160,7 @@ class HandleCollisionsAction(Action):
             #food = cast.get_first_actor("foods")
             obstacle = cast.get_first_actor("obstacles")
             food = cast.get_first_actor("foods")
-            
+
             x = int(constants.MAX_X / 2)
             y = int(constants.MAX_Y / 2)
             position = Point(x, y)
@@ -174,19 +177,19 @@ class HandleCollisionsAction(Action):
                 message.set_color(constants.YELLOW)
 
                 message.set_text("Game Over! It was a tie!")
-                
+
             message.set_position(position)
             cast.add_actor("messages", message)
 
-            
-            
-            
+
+
+
             for segment in segments:
                 segment.set_color(constants.WHITE)
             snake.color_two = constants.WHITE
             food.set_color(constants.WHITE)
             obstacle.set_color(constants.WHITE)
-            
+
             for segment in segments2:
                 segment.set_color(constants.WHITE)
             snake2.color_two = constants.WHITE
--- game/shared/point.py
+++ game/shared/point.py
@@ -44,6 +44,17 @@ class Point:
             boolean: True if both x and y are equal; false if otherwise.
         """
         return self._x == other.get_x() and self._y == other.get_y()
+ 
+    def equals2(self, y):
+        """Whether or not this Point is equal to the given one.
+
+        Args:
+            other (Point): The Point to compare.
+
+        Returns: 
+            boolean: True if both x and y are equal; false if otherwise.
+        """
+        return self._y == y
 
     def get_x(self):
         """Gets the horizontal distance.
