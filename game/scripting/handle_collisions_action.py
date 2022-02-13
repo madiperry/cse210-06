65868ddef151c66b9870744d69193aae2cbf490a
JermainLopez <97272097+JermainLopez@users.noreply.github.com>
2022-02-12 22:07:31 -0600

    Update handle_collisions_action.py

--- game/scripting/handle_collisions_action.py
+++ game/scripting/handle_collisions_action.py
@@ -7,7 +7,7 @@ from game.shared.point import Point
 class HandleCollisionsAction(Action):
     """
     An update action that handles interactions between the actors.
-
+    
     The responsibility of HandleCollisionsAction is to handle the situation when the snake collides
     with the food, or the snake collides with its segments, or the game is over.
 
@@ -31,37 +31,199 @@ class HandleCollisionsAction(Action):
             self._handle_food_collision(cast)
             self._handle_segment_collision(cast)
             self._handle_game_over(cast)
-            self._handle_top_collision(cast)
 
     def _handle_food_collision(self, cast):
         """Updates the score nd moves the food if the snake collides with the food.
-
+        
         Args:
             cast (Cast): The cast of Actors in the game.
         """
         score1 = cast.get_first_actor("scores")
         score2 = cast.get_second_actor("scores")
         nitro = cast.get_first_actor("nitros")
+        food = cast.get_first_actor("foods")
+        food2 = cast.get_second_actor("foods") #ADDED
         obstacle = cast.get_first_actor("obstacles")
+        obstacle2 = cast.get_second_actor("obstacles") #ADDED
         snake = cast.get_first_actor("snakes")
         snake2 = cast.get_second_actor("snakes")   # added
         head = snake.get_segments()[0]
         head2 = snake2.get_segments()[0]   # added
-
+        
         if head.get_position().equals(nitro.get_position()):
             nitro.accelerate()
             nitro.reset()
-        if head2.get_position().equals(nitro.get_position()):
+        elif head2.get_position().equals(nitro.get_position()):
             nitro.accelerate()
             nitro.reset()
-        if head.get_position().equals(obstacle.get_position()):
-            obstacle.accelerate()
+        elif head.get_position().equals(food.get_position()):
+            points = food.get_points()                          # added     
+            score1.add_points(points)                                # added       
+            food.reset() 
+            food2.reset() 
+            obstacle.reset()                                     # added     
+            obstacle2.reset()     
+        elif head2.get_position().equals(food.get_position()):
+            points = food.get_points()                          # added     
+            score2.add_points(points)                                # added       
+            food.reset()  
+            food2.reset() 
+            obstacle.reset() 
+            obstacle2.reset() 
+        elif head.get_position().equals(food2.get_position()):
+            points = food2.get_points()                          # added     
+            score1.add_points(points)                                # added       
+            food.reset() 
+            food2.reset() 
+            obstacle.reset()                                # added   
+            obstacle2.reset()       
+        elif head2.get_position().equals(food2.get_position()):
+            points = food2.get_points()                          # added     
+            score2.add_points(points)                                # added       
+            food.reset() 
+            food2.reset() 
+            obstacle.reset() 
+            obstacle2.reset() 
+        elif head2.get_position().equals(obstacle.get_position()):   # added
+            points = obstacle.get_points()                          # added     
+            score2.add_points(points)                                # added       
+            food.reset() 
+            food2.reset() 
             obstacle.reset()
+            obstacle2.reset() 
+        elif head.get_position().equals(obstacle.get_position()):   #cahnged head2
+            points = obstacle.get_points()                          # added     
+            score1.add_points(points)                                # added       
+            obstacle.reset() 
+            food.reset() 
+            food2.reset() 
+            obstacle.reset()    
+            obstacle2.reset()                                  # added  
+        elif head.get_position().equals(obstacle2.get_position()):
+            points = food2.get_points()                          # added     
+            score1.add_points(points)                                # added       
+            food.reset() 
+            food2.reset() 
+            obstacle.reset()  
+            obstacle2.reset()                               # added         
+        elif head2.get_position().equals(obstacle2.get_position()):
+            points = food2.get_points()                          # added     
+            score2.add_points(points)                                # added       
+            food.reset() 
+            food2.reset() 
+            obstacle.reset() 
+            obstacle2.reset()    
 
-        if head2.get_position().equals(obstacle.get_position()):   # added
-            points = obstacle.get_points()                          # added
-            score2.add_points(points)                                # added
-            obstacle.reset()                                        # added
+    def _handle_top_collision(self, cast):
+        """Updates the score when car collides with the top of screen.
+        
+        Args:
+            cast (Cast): The cast of Actors in the game.
+        """
+        score1 = cast.get_first_actor("scores")
+        score2 = cast.get_second_actor("scores")
+        snake = cast.get_first_actor("snakes")
+        snake2 = cast.get_second_actor("snakes")  
+        top = constants.MAX_Y   #CAHNGED
+       
+        
+        if snake.get_position().equals(top):
+             points = top.get_points()
+             score1.add_points(points)
+             
+        if snake2.get_position().equals(top):
+             points = top.get_points()
+             score2.add_points(points)
+
+    def _handle_segment_collision(self, cast):
+        """Sets the game over flag if the snake collides with one of its segments.
+        
+        Args:
+            cast (Cast): The cast of Actors in the game.
+        """
+        score1 = cast.get_first_actor("scores")
+        score2 = cast.get_second_actor("scores")
+        snake = cast.get_first_actor("snakes")
+        snake2 = cast.get_second_actor("snakes") # added
+
+        head = snake.get_head()
+        head2 = snake2.get_head() # added
+
+        segments = snake.get_segments()[1:]
+        segments2 = snake2.get_segments()[1:] # added
+
+        for segment in segments:
+            if head2.get_position().equals(head.get_position()):     # If snake 2 hits snake 1 
+                score2.add_points(50)
+
+
+        for segment in segments2:                                      # added
+            if head.get_position().equals(head.get_position()):     # if snake 1 hits snake 2
+                score1.add_points(50)
+
+    def _handle_game_over(self, cast):
+        """Shows the 'game over' message and turns the snake and food white if the game is over.
+        
+        Args:
+            cast (Cast): The cast of Actors in the game.
+        """
+        
+        if self._is_game_over:
+            snake = cast.get_first_actor("snakes")
+            snake2 = cast.get_second_actor("snakes") # added
+
+            segments2 = snake2.get_segments() # added
+            segments = snake.get_segments()
+            #food = cast.get_first_actor("foods")
+            obstacle = cast.get_first_actor("obstacles")
+            food = cast.get_first_actor("foods")
+            
+            x = int(constants.MAX_X / 2)
+            y = int(constants.MAX_Y / 2)
+            position = Point(x, y)
+
+            message = Actor()
+            if self.winner == 1:
+                message.set_color(constants.RED)
+                message.set_text("Game Over! Player 1 wins!")
+            if self.winner == 2:
+                message.set_color(constants.BLUE)
+
+                message.set_text("Game Over! Player 2 wins!")
+            if self.winner == 3:
+                message.set_color(constants.YELLOW)
+
+                message.set_text("Game Over! It was a tie!")
+                
+            message.set_position(position)
+            cast.add_actor("messages", message)
+
+            
+            
+            
+            for segment in segments:
+                segment.set_color(constants.WHITE)
+            snake.color_two = constants.WHITE
+            food.set_color(constants.WHITE)
+            obstacle.set_color(constants.WHITE)
+            
+            for segment in segments2:
+                segment.set_color(constants.WHITE)
+            snake2.color_two = constants.WHITE
+            
+    def execute(self, cast, script):
+        """Executes the handle collisions action.
+
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game.
+        """
+        if not self._is_game_over:
+            self._handle_food_collision(cast)
+            self._handle_segment_collision(cast)
+            self._handle_game_over(cast)
+            self._handle_top_collision(cast)
+
 
     def _handle_top_collision(self, cast):
         """Updates the score when car collides with the top of screen.
@@ -79,117 +241,9 @@ class HandleCollisionsAction(Action):
         top = 0
         
         if head.get_position().equals2(top):
-                points = 1
+                points = 100
                 score1.add_points(points)
 
         if head2.get_position().equals2(top):
-                points = 1
+                points = 100
                 score2.add_points(points)
-
-    def _handle_segment_collision(self, cast):
-        """Sets the game over flag if the snake collides with one of its segments.
-
-        Args:
-            cast (Cast): The cast of Actors in the game.
-        """
-        score1 = cast.get_first_actor("scores")
-        score2 = cast.get_second_actor("scores")
-        snake = cast.get_first_actor("snakes")
-        snake2 = cast.get_second_actor("snakes") # added
-
-        head = snake.get_head()
-        head2 = snake2.get_head() # added
-
-        segments = snake.get_segments()[1:]
-        segments2 = snake2.get_segments()[1:] # added
-
-        for segment in segments:
-            if head2.get_position().equals(segment.get_position()):     # If snake 2 hits snake 1
-                score2.add_points(-50)
-                self._is_game_over = True
-                if score1.get_points() > score2.get_points():
-                    self.winner = 1
-                elif score1.get_points() == score2.get_points():
-                    self.winner = 3
-                else:
-                    self.winner = 2
-            if head.get_position().equals(segment.get_position()):      # if snake 1 hits itself
-                score1.add_points(-50)
-                self._is_game_over = True
-                if score1.get_points() > score2.get_points():
-                    self.winner = 1
-                elif score1.get_points() == score2.get_points():
-                    self.winner = 3
-                else:
-                    self.winner = 2
-
-        for segment in segments2:                                      # added
-            if head.get_position().equals(segment.get_position()):     # if snake 1 hits snake 2
-                score1.add_points(-50)
-                self._is_game_over = True
-                if score1.get_points() > score2.get_points():
-                    self.winner = 1
-                elif score1.get_points() == score2.get_points():
-                    self.winner = 3
-                else:
-                    self.winner = 2
-            if head2.get_position().equals(segment.get_position()):    # if snake 2 hits itself
-                score2.add_points(-50)
-                self._is_game_over = True
-                if score1.get_points() > score2.get_points():
-                    self.winner = 1
-                elif score1.get_points() == score2.get_points():
-                    self.winner = 3
-                else:
-                    self.winner = 2
-
-
-    def _handle_game_over(self, cast):
-        """Shows the 'game over' message and turns the snake and food white if the game is over.
-
-        Args:
-            cast (Cast): The cast of Actors in the game.
-        """
-
-        if self._is_game_over:
-            snake = cast.get_first_actor("snakes")
-            snake2 = cast.get_second_actor("snakes") # added
-
-            segments2 = snake2.get_segments() # added
-            segments = snake.get_segments()
-            #food = cast.get_first_actor("foods")
-            obstacle = cast.get_first_actor("obstacles")
-            food = cast.get_first_actor("foods")
-
-            x = int(constants.MAX_X / 2)
-            y = int(constants.MAX_Y / 2)
-            position = Point(x, y)
-
-            message = Actor()
-            if self.winner == 1:
-                message.set_color(constants.RED)
-                message.set_text("Game Over! Player 1 wins!")
-            if self.winner == 2:
-                message.set_color(constants.BLUE)
-
-                message.set_text("Game Over! Player 2 wins!")
-            if self.winner == 3:
-                message.set_color(constants.YELLOW)
-
-                message.set_text("Game Over! It was a tie!")
-
-            message.set_position(position)
-            cast.add_actor("messages", message)
-
-
-
-
-            for segment in segments:
-                segment.set_color(constants.WHITE)
-            snake.color_two = constants.WHITE
-            food.set_color(constants.WHITE)
-            obstacle.set_color(constants.WHITE)
-
-            for segment in segments2:
-                segment.set_color(constants.WHITE)
-            snake2.color_two = constants.WHITE
